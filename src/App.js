c8ab190eed97bfaeb19b4fb17e0c163fcc97475a
Al <asmaldone@usnews.com>
2021-09-27 16:18:35 -0400

    inti push

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import './App.css';
 import {BrowserRouter, Route, Switch} from "react-router-dom";
 import Homepage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 
-const HatesPage = () => (
-    <div><h1>Hats Page</h1></div>
-)
 
 function App() {
     return (
@@ -12,7 +10,7 @@ function App() {
             <div>
                 <Switch>
                     <Route exact={true} path='/' component={Homepage}/>
-                    <Route path='/hats' component={HatesPage}/>
+                    <Route path='/shop' component={ShopPage}/>
                 </Switch>
             </div>
         </BrowserRouter>
