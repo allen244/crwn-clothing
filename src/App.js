1bc02b61dc69d5634628a65b6bedeab37dff7a3c
Al <asmaldone@usnews.com>
2021-10-04 14:17:31 -0400

    inti push

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 import {BrowserRouter, Route, Switch} from "react-router-dom";
 import Homepage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
-
+import Header from "./components/header/header.component";
 
 function App() {
     return (
         <BrowserRouter>
             <div>
+                <Header/>
                 <Switch>
                     <Route exact={true} path='/' component={Homepage}/>
                     <Route path='/shop' component={ShopPage}/>
