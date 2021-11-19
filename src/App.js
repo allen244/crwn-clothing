bb6e10273fa16085f67cd8aa6131c5b7ec599f99
asmaldone <alsmaldone@yahoo.com>
2021-11-19 16:48:48 -0500

    SIGN IN GOOLGE FIREBASE

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Header from "./components/header/header.component";
 import {auth} from "./firebase/firebase.utils"
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
+import {createUserProfileDocument} from "./firebase/firebase.utils";
+
 class App extends React.Component {
     constructor() {
         super();
@@ -18,11 +20,26 @@ class App extends React.Component {
     unsubscribeFromAuth = null;
 
     componentDidMount() {
-        this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-            this.setState({currentUser: user});
-            console.log(user)
-        })
+        this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+            if (userAuth) {
+                const userRef = await createUserProfileDocument(userAuth);
+
+                userRef.onSnapshot(snapShot => {
+                    this.setState({
+                        currentUser: {
+                            id: snapShot.id,
+                            ...snapShot.data()
+                        }
+                    });
+
+                    console.log(this.state);
+                });
+            }
+
+            this.setState({ currentUser: userAuth });
+        });
     }
+
 
     componentWillUnmount() {
         this.unsubscribeFromAuth();
