0e0f4187614d605a4de120f8b3f781ff5b9184b7
asmaldone <alsmaldone@yahoo.com>
2021-11-20 17:41:27 -0500

    REDUX

--- src/App.js
+++ src/App.js
@@ -1,42 +1,35 @@
 import './App.css';
 import React from "react";
 import {BrowserRouter, Route, Switch} from "react-router-dom";
+import {connect} from "react-redux";
+import {setCurrentUser} from "./redux/user/user.actions";
 import Homepage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import {auth} from "./firebase/firebase.utils"
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
-
 import {createUserProfileDocument} from "./firebase/firebase.utils";
 
 class App extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            currentUser: null
-        }
-    }
 
     unsubscribeFromAuth = null;
 
     componentDidMount() {
+        const { setCurrentUser } = this.props;
+
         this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
             if (userAuth) {
                 const userRef = await createUserProfileDocument(userAuth);
 
                 userRef.onSnapshot(snapShot => {
-                    this.setState({
-                        currentUser: {
-                            id: snapShot.id,
-                            ...snapShot.data()
-                        }
+                    setCurrentUser({
+                        id: snapShot.id,
+                        ...snapShot.data()
                     });
-
-                    console.log(this.state);
                 });
             }
 
-            this.setState({ currentUser: userAuth });
+            setCurrentUser(userAuth);
         });
     }
 
@@ -49,7 +42,7 @@ class App extends React.Component {
         return (
             <BrowserRouter>
                 <div>
-                    <Header currentUser={this.state.currentUser}/>
+                    <Header/>
                     <Switch>
                         <Route exact={true} path='/' component={Homepage}/>
                         <Route path='/shop' component={ShopPage}/>
@@ -64,4 +57,8 @@ class App extends React.Component {
 
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+    setCurrentUser: user => dispatch(setCurrentUser(user))
+
+})
+export default connect(null, mapDispatchToProps)(App);
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import './sign-in.styles.scss';
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
-import {signInWithGoolge} from "../../firebase/firebase.utils";
+import {auth, signInWithGoolge} from "../../firebase/firebase.utils";
 
 
 class SignIn extends React.Component {
@@ -13,32 +13,51 @@ class SignIn extends React.Component {
         this.state = {
             email: '',
             password: ''
-        }
+        };
     }
 
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault();
-        this.setState({email: '', password: ''})
-    }
+
+        const { email, password } = this.state;
+
+        try {
+            await auth.signInWithEmailAndPassword(email, password);
+            this.setState({ email: '', password: '' });
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
     handleChange = event => {
-        const {value, name} = event.target;
-        this.setState({
-                [name]: value
-            }
-        )
-    }
+        const { value, name } = event.target;
+
+        this.setState({ [name]: value });
+    };
 
     render() {
         return (
             <div className='sign-in'>
                 <h2>I already have an account</h2>
                 <span>Sign in with your email and password</span>
-                <form onSubmit={this.handleSubmit}>
-                    <FormInput name="email" type='email' value={this.state.email} label='email' required
-                               handleChange={this.handleChange}/>
 
-                    <FormInput name="password" type="password" label='password' value={this.state.password} required
-                               handleChange={this.handleChange}/>
+                <form onSubmit={this.handleSubmit}>
+                    <FormInput
+                        name='email'
+                        type='email'
+                        handleChange={this.handleChange}
+                        value={this.state.email}
+                        label='email'
+                        required
+                    />
+                    <FormInput
+                        name='password'
+                        type='password'
+                        value={this.state.password}
+                        handleChange={this.handleChange}
+                        label='password'
+                        required
+                    />
                     <div className='buttons'>
                         <CustomButton type='submit'> Sign in </CustomButton>
                         <CustomButton onClick={signInWithGoolge} isGoogleSignIn>
@@ -46,9 +65,8 @@ class SignIn extends React.Component {
                         </CustomButton>
                     </div>
                 </form>
-
             </div>
-        )
+        );
     }
 }
 
