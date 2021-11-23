9803861252e99c604c7931b1f161c8c84120c089
asmaldone <alsmaldone@yahoo.com>
2021-11-23 15:00:27 -0500

    cart selectors

--- src/components/cart-dropdown/cart-dropdown.compenent.jsx
+++ src/components/cart-dropdown/cart-dropdown.compenent.jsx
@@ -1,15 +1,23 @@
 import React from "react";
 import CustomButton from "../custom-button/custom-button.component";
 import './cart-dropdown.styles.scss';
+import CartItem from "../cart-item/cart-item.component";
+import {connect} from "react-redux";
+import {selectCartItems} from "../../redux/cart/cart.selectors";
 
-
-const CartDropDown = () => (
+const CartDropDown = ({cartItems}) => (
     <div className='cart-dropdown'>
-
         <div className='cart-items'>
-            <CustomButton>GO TO CHECKOUT</CustomButton>
+            {cartItems.map(cartItem => (
+                <CartItem key={cartItem.id} item={cartItem}/>
+            ))}
         </div>
+        <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
 );
 
-export default CartDropDown;
+const mapStateToProps = state => ({
+    cartItems: selectCartItems(state)
+});
+
+export default connect(mapStateToProps)(CartDropDown);
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -34,7 +34,9 @@ const Header = ({currentUser, hidden}) => (
     </div>
 )
 
-const mapStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({
+
+
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
     currentUser,
     hidden
 });
